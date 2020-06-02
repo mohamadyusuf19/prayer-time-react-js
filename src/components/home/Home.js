bb3563974617c919b64c2b62ee695f1b370091b8
AhmadDev <yusufadinata19@gmail.com>
2020-06-03 05:31:05 +0700

    merge dhimas

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -79,6 +79,7 @@ const Home = ({
   // console.log(time);
   const conditional = () => {
     const date = new Date();
+    console.log(getHourAndMinutes(subuh));
     const compareDate = (firstTime, lastTime) => {
       // eslint-disable-next-line no-unused-expressions
       return (
@@ -113,64 +114,64 @@ const Home = ({
   };
   return (
     <div className='wrapper-home'>
-      <div className="inline-wrapper-home">
-      <div className='card'>
-        <p className='text-title-iftar'>Waktu Berbuka</p>
-        <p className='text-region-iftar'>
-          <FaMapMarker /> {region}
+      <div className='inline-wrapper-home'>
+        <div className='card'>
+          <p className='text-title-iftar'>Waktu Berbuka</p>
+          <p className='text-region-iftar'>
+            <FaMapMarker /> {region}
+          </p>
+          {loading ? (
+            <Skeleton count={2} />
+          ) : (
+            <div>
+              <p className='text-date-iftar-hijri'>{dateRamadhan}</p>
+              <p className='text-date-iftar'>{date}</p>
+            </div>
+          )}
+          {loading ? (
+            <Skeleton height={40} width={"50%"} />
+          ) : (
+            <p className='text-time-iftar'>{magrib}</p>
+          )}
+        </div>
+        {conditional()}
+        <div className='card' onClick={() => history.push("/doa-buka-puasa")}>
+          <img alt='icon' src={mosqueIcon} className='mosque' />
+          <p className='text-title-iftar'>Lupa doa berbuka puasa ?</p>
+          <p className='text-region-iftar'>Yuk hafalin</p>
+        </div>
+        <p className='title-schedule'>Jadwal Sholat</p>
+        <p className='desc'>
+          Jangan sampai dehidrasi membuat anda lupa waktu sholat ya
         </p>
         {loading ? (
-          <Skeleton count={2} />
+          <div className='wrapper-time-skeleton'>
+            <Skeleton count={5} height={40} />
+          </div>
         ) : (
-          <div>
-            <p className='text-date-iftar-hijri'>{dateRamadhan}</p>
-            <p className='text-date-iftar'>{date}</p>
-          </div>
+          <>
+            <div className='wrapper-time'>
+              <p className='text-time'>Subuh</p>
+              <p className='text-date'>{subuh}</p>
+            </div>
+            <div className='wrapper-time'>
+              <p className='text-time'>Zuhur</p>
+              <p className='text-date'>{zuhur}</p>
+            </div>
+            <div className='wrapper-time'>
+              <p className='text-time'>Ashar</p>
+              <p className='text-date'>{ashar}</p>
+            </div>
+            <div className='wrapper-time'>
+              <p className='text-time'>Maghrib</p>
+              <p className='text-date'>{magrib}</p>
+            </div>
+            <div className='wrapper-time'>
+              <p className='text-time'>Isya'</p>
+              <p className='text-date'>{isya}</p>
+            </div>
+          </>
         )}
-        {loading ? (
-          <Skeleton height={40} width={"50%"} />
-        ) : (
-          <p className='text-time-iftar'>{magrib}</p>
-        )}
-      </div>
-      {conditional()}
-      <div className='card' onClick={() => history.push("/doa-buka-puasa")}>
-        <img alt='icon' src={mosqueIcon} className='mosque' />
-        <p className='text-title-iftar'>Lupa doa berbuka puasa ?</p>
-        <p className='text-region-iftar'>Yuk hafalin</p>
-      </div>
-      <p className='title-schedule'>Jadwal Sholat</p>
-      <p className='desc'>
-        Jangan sampai dehidrasi membuat anda lupa waktu sholat ya
-      </p>
-      {loading ? (
-        <div className='wrapper-time-skeleton'>
-          <Skeleton count={5} height={40} />
-        </div>
-      ) : (
-        <>
-          <div className='wrapper-time'>
-            <p className='text-time'>Subuh</p>
-            <p className='text-date'>{subuh}</p>
-          </div>
-          <div className='wrapper-time'>
-            <p className='text-time'>Zuhur</p>
-            <p className='text-date'>{zuhur}</p>
-          </div>
-          <div className='wrapper-time'>
-            <p className='text-time'>Ashar</p>
-            <p className='text-date'>{ashar}</p>
-          </div>
-          <div className='wrapper-time'>
-            <p className='text-time'>Maghrib</p>
-            <p className='text-date'>{magrib}</p>
-          </div>
-          <div className='wrapper-time'>
-            <p className='text-time'>Isya'</p>
-            <p className='text-date'>{isya}</p>
-          </div>
-        </>
-      )}
       </div>
     </div>
   );
