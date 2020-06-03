0e6595bd5995f1745bd14b15b37cdd96f6d14572
AhmadDev <yusufadinata19@gmail.com>
2020-06-03 09:44:05 +0700

    comment onfocus footer and merge map

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,27 +5,27 @@ import { OnFocusContext } from "../../context/onFocusMapContext";
 import "./footer.scss";
 
 const Footer = () => {
-  const { onFocus } = useContext(OnFocusContext);
-  React.useEffect(() => {
-    console.log("coba", onFocus);
-  });
+  // const { onFocus } = useContext(OnFocusContext);
+  // React.useEffect(() => {
+  //   console.log("coba", onFocus);
+  // });
 
   return (
     // <OnFocusContext.Consumer>
     //   {(OnFocus) => (
 
-    <div className="wrapper-footer">
-      <footer className={onFocus === false ? "footer" : "footerTop"}>
-        <NavLink exact className="icon" to="/" activeClassName="active-icon">
+    <div className='wrapper-footer'>
+      <footer className='footer'>
+        <NavLink exact className='icon' to='/' activeClassName='active-icon'>
           <FaHome />
         </NavLink>
-        <NavLink className="icon" to="/maps" activeClassName="active-icon">
+        <NavLink className='icon' to='/maps' activeClassName='active-icon'>
           <FaMapMarker />
         </NavLink>
-        <NavLink className="icon" to="/schedules" activeClassName="active-icon">
+        <NavLink className='icon' to='/schedules' activeClassName='active-icon'>
           <FaTable />
         </NavLink>
-        <NavLink className="icon" to="/account" activeClassName="active-icon">
+        <NavLink className='icon' to='/account' activeClassName='active-icon'>
           <FaInfoCircle />
         </NavLink>
       </footer>
