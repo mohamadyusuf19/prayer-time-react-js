b9d5e63e7d837ef7042b6e23d211715b09fd170f
AhmadDev <yusufadinata19@gmail.com>
2020-06-25 06:47:52 +0700

    update home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,21 +1,14 @@
-import React, { useState, useEffect } from "react";
-import isEmpty from "lodash/isEmpty";
-import isNil from "lodash/isNil";
-import { useHistory } from "react-router-dom";
+import React from "react";
 import Skeleton from "react-loading-skeleton";
 import { FaMapMarker } from "react-icons/fa";
-import moment from "moment";
 
 import "./home.scss";
 import {
   getHourAndMinutes,
   unixTime,
   formatDateCompare,
-  setDurationTime,
 } from "../../utils/FormatDate";
 import Time from "../time/Time";
-
-const mosqueIcon = require("../../assets/mosque-light.png");
 
 const Home = ({
   subuh,
@@ -29,54 +22,6 @@ const Home = ({
   loading,
   dateNow,
 }) => {
-  const history = useHistory();
-  // const [time, setTime] = useState({
-  //   hours: 0,
-  //   minutes: 0,
-  //   seconds: 0,
-  // });
-  // const durationTime = setDurationTime(magrib);
-  // const hoursDurations = isEmpty(durationTime)
-  //   ? null
-  //   : durationTime.hoursDurations;
-  // const minutesDurations = isEmpty(durationTime)
-  //   ? null
-  //   : durationTime.minutesDurations;
-  // console.log("hourduration", hoursDurations);
-  // // useEffect(() => {
-  // //   let timer = setInterval(() => showRemaining(), 1000);
-  // //   return () => {
-  // //     clearInterval(timer);
-  // //   };
-  // // }, []);
-  // const setTimer = () => {
-  //   let dates = new Date();
-  //   let end = moment(dates)
-  //     .add(hoursDurations, "hour")
-  //     .add(minutesDurations, "minute")
-  //     .add(10, "seconds");
-  // };
-  // const showRemaining = () => {
-  //   let date = new Date();
-  //   let now = date;
-  //   let distance = end - now;
-  //   if (distance < 0) {
-  //     return clearInterval();
-  //   }
-  //   let timeDay = 1000 * 60 * 60 * 24;
-  //   let timeHours = 1000 * 60 * 60;
-  //   let timeMinutes = 1000 * 60;
-  //   let timeSeconds = 1000;
-  //   let hours = Math.floor((distance % timeDay) / timeHours);
-  //   let minutes = Math.floor((distance % timeHours) / timeMinutes);
-  //   let seconds = Math.floor((distance % timeMinutes) / timeSeconds);
-  //   setTime({
-  //     hours,
-  //     minutes,
-  //     seconds,
-  //   });
-  // };
-  // console.log(time);
   const newDate = new Date();
   const compareDate = (firstTime, lastTime) => {
     // eslint-disable-next-line no-unused-expressions
@@ -113,7 +58,6 @@ const Home = ({
       return <Time prayerTime='Isya' realTime={isya} />;
     }
   };
-  const background = `linear-gradient(to right, #055a91, rgba(50, 160, 160, 0.938))`;
   return (
     <div className='wrapper-home'>
       <div className='inline-wrapper-home'>
