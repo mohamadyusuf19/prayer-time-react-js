7873a2be38e35e56f9e998fb93bd578dc3fb201b
mohamadyusuf19 <yusufadinata19@gmail.com>
2021-01-25 22:19:19 +0700

    update schedules full 1 month

--- src/container/ScheduleContainer.js
+++ src/container/ScheduleContainer.js
@@ -1,6 +1,4 @@
 import React from "react";
-import isEmpty from "lodash/isEmpty";
-import moment from "moment";
 
 import Layout from "../components/layout/Layout";
 import Schedules from "../components/schedules/Schedules";
@@ -8,20 +6,13 @@ import { SchedulesConsumer } from "../context/schedulesPrayerContext";
 
 const ScheduleContainer = () => {
   window.scrollTo(0, 0);
-  const date = new Date();
-  const formatDate = moment(date).format("DD-MM-YYYY");
   return (
     <Layout>
       <SchedulesConsumer>
         {(context) => {
-          const newData = isEmpty(context)
-            ? null
-            : context.state.data.filter(
-                (item) => item.date.gregorian.date >= formatDate
-              );
           return (
             <Schedules
-              dataSource={newData}
+              dataSource={context.state.data}
               region={context.state.region.label}
               loading={context.state.loading}
             />
